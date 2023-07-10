37c70fe297ff3edcc155b21fd395e9ab84ab0bfa
Vitória <vick_santos_vick@hotmail.com>
2023-07-10 02:02:36 -0300

    --- montando  a home page

--- lib/presentation/views/home/home_page.dart
+++ lib/presentation/views/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pov/presentation/widgets/bottom_navigation.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,127 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: const Text(
+              "Home",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+            ),
+          ),
+          backgroundColor: Colors.white,
+          bottomNavigationBar: const BottomNavigation(),
+          body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+              margin: EdgeInsets.all(15),
+              width: size.width,
+              height: 180,
+              decoration: BoxDecoration(
+                  color: Colors.pinkAccent,
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Text(
+                "Destaques da semana:",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    fontStyle: FontStyle.italic,
+                    color: Color(0xFF393434)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Stack(children: [
+                Container(
+                  width: 180,
+                  height: 260,
+                  decoration: BoxDecoration(
+                      color: Color(0xFFF8F8F8),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 160,
+                        height: 160,
+                        margin: EdgeInsets.only(top: 10, left: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.purpleAccent[700],
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      SizedBox(height: 5),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 30,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                  color: Colors.blueGrey,
+                                  borderRadius: BorderRadius.circular(15)),
+                            ),
+                            const Column(
+                              children: [
+                                Text("ana"),
+                                Text(
+                                  "Praia de Itapuã, Salvador",
+                                  maxLines: 2,
+                                  style: TextStyle(fontSize: 9),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 15,
+                  right: 15,
+                  child: Container(
+                    width: 60,
+                    height: 30,
+                    decoration: BoxDecoration(
+                        boxShadow: const [
+                          BoxShadow(
+                              offset: Offset(0, 4),
+                              blurRadius: 4,
+                              color: Color.fromARGB(33, 0, 0, 0))
+                        ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "5",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.pink),
+                        ),
+                        SizedBox(width: 5),
+                        Icon(
+                          Icons.star,
+                          size: 18,
+                          color: Colors.pink,
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ]),
+            )
+          ])),
+    );
   }
 }
