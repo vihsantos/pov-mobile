973adf72f0fb067b2910398252892c01b79c655a
Vitória <vick_santos_vick@hotmail.com>
2023-07-10 02:02:59 -0300

    montando a página de cadastro

--- lib/presentation/views/register/register_page.dart
+++ lib/presentation/views/register/register_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/input_field.dart';
+
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
@@ -10,6 +12,84 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            "Cadastro",
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+          ),
+        ),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              InputField(
+                label: 'Nome',
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: "Digite seu nome"),
+                ),
+              ),
+              InputField(
+                label: 'Email',
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: "Digite seu email"),
+                ),
+              ),
+              InputField(
+                label: 'Nome de usuário',
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: "Digite um nome de usuário"),
+                ),
+              ),
+              InputField(
+                label: 'Data de nascimento',
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: "Digite sua data de nascimento"),
+                ),
+              ),
+              InputField(
+                label: 'Senha',
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: "Digite uma senha"),
+                ),
+              ),
+              InputField(
+                label: 'Confirme sua senha',
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: "Confirme sua senha"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
