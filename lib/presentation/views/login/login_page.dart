7c052388c67c5febc88ed71fb6ed2ecd2656295a
Vitória <vick_santos_vick@hotmail.com>
2023-07-10 22:09:12 -0300

    --- ajustando a página de login

--- lib/presentation/views/login/login_page.dart
+++ lib/presentation/views/login/login_page.dart
@@ -16,62 +16,68 @@ class _LoginPageState extends State<LoginPage> {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 120,
-              ),
-              Image.asset(
-                "asset/img/logo__pov.png",
-                width: 250,
-              ),
-              const InputField(
-                  label: 'Email',
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        hintText: "Digite seu email"),
-                  )),
-              const InputField(
-                  label: 'Senha',
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        hintText: "Digite sua senha"),
-                  )),
-              InkWell(
-                onTap: null,
-                child: Container(
-                  margin: EdgeInsets.all(15),
-                  width: size.width,
-                  height: 65,
-                  decoration: const BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                            offset: Offset(0, 4),
-                            blurRadius: 4,
-                            color: Color.fromARGB(55, 116, 116, 116))
-                      ],
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [Color(0xFFE86060), Color(0xFFE86192)])),
-                  child: const Center(
-                      child: Text(
-                    "Entrar",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600),
-                  )),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 120,
                 ),
-              )
-            ],
+                Image.asset(
+                  "asset/img/logo__pov.png",
+                  width: 250,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const InputField(
+                    label: 'Email',
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          hintText: "Digite seu email"),
+                    )),
+                const InputField(
+                    label: 'Senha',
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          hintText: "Digite sua senha"),
+                    )),
+                InkWell(
+                  onTap: null,
+                  child: Container(
+                    margin: EdgeInsets.all(15),
+                    width: size.width,
+                    height: 60,
+                    decoration: const BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              offset: Offset(0, 4),
+                              blurRadius: 4,
+                              color: Color.fromARGB(55, 116, 116, 116))
+                        ],
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [Color(0xFFE86060), Color(0xFFE86192)])),
+                    child: const Center(
+                        child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    )),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
