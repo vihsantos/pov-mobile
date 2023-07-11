cbfe4a7d6fa060148cf94c765d1b89089bbe4f4b
Vitória <vick_santos_vick@hotmail.com>
2023-07-10 22:08:55 -0300

    --- montando o esboço da página de grupos

--- lib/presentation/views/groups/groups_page.dart
+++ lib/presentation/views/groups/groups_page.dart
@@ -10,6 +10,19 @@ class GroupsPage extends StatefulWidget {
 class _GroupsPageState extends State<GroupsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            "Grupos",
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+          ),
+        ),
+        body: Padding(padding: const EdgeInsets.all(15), child: Container()),
+      ),
+    );
   }
 }
