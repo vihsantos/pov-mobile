70066b9847eabe25a90139426d6da12caa6043e5
Vitória <vick_santos_vick@hotmail.com>
2023-07-12 22:52:30 -0300

    --- implementando o mapa na home page

--- lib/presentation/views/home/home_page.dart
+++ lib/presentation/views/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:pov/presentation/widgets/bottom_navigation.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,41 +27,55 @@ class _HomePageState extends State<HomePage> {
           ),
           backgroundColor: Colors.white,
           bottomNavigationBar: const BottomNavigation(),
-          body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Container(
-              margin: const EdgeInsets.all(15),
-              width: size.width,
-              height: 180,
-              decoration: BoxDecoration(
-                  color: Colors.pinkAccent,
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Destaques da semana:",
-                style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    fontStyle: FontStyle.italic,
-                    color: Color(0xFF393434)),
-              ),
-            ),
-            const Padding(
-              padding: const EdgeInsets.all(15),
-              child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          body: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Container(
+                margin: const EdgeInsets.only(
+                    left: 15, right: 15, bottom: 10, top: 10),
+                height: 180,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: FlutterMap(
+                    options: MapOptions(
+                        center: const LatLng(-12.9704, -38.5124), zoom: 15),
                     children: [
-                      CardDestaque(),
-                      CardDestaque(),
-                      CardDestaque(),
-                      CardDestaque(),
+                      TileLayer(
+                        urlTemplate:
+                            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        userAgentPackageName: 'com.example.app',
+                      )
                     ],
-                  )),
-            )
-          ])),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Text(
+                  "Destaques da semana:",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      fontStyle: FontStyle.italic,
+                      color: Color(0xFF393434)),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(15),
+                child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CardDestaque(),
+                        CardDestaque(),
+                        CardDestaque(),
+                        CardDestaque(),
+                      ],
+                    )),
+              )
+            ]),
+          )),
     );
   }
 }
