5fd58fc0f25026388a2b91e68f32449f9bb16ad2
Vitória <vick_santos_vick@hotmail.com>
2023-07-16 21:56:29 -0300

    --- criando a page postdetails

---
+++ lib/presentation/views/post/postdetails_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PostDetailsPage extends StatefulWidget {
+  const PostDetailsPage({super.key});
+
+  @override
+  State<PostDetailsPage> createState() => _PostDetailsPageState();
+}
+
+class _PostDetailsPageState extends State<PostDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
