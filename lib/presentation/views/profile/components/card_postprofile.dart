0082ba547a4d2caf90fb73aef50afbf8f20355b5
Vitória <vick_santos_vick@hotmail.com>
2023-07-24 23:28:07 -0300

    --- ajustando o card profile

--- lib/presentation/views/profile/components/card_postprofile.dart
+++ lib/presentation/views/profile/components/card_postprofile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../post/postdetails_page.dart';
+
 class CardPostProfile extends StatelessWidget {
   const CardPostProfile({
     super.key,
@@ -7,48 +9,54 @@ class CardPostProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Stack(children: [
-        Container(
-          decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 255, 227, 225),
-              borderRadius: BorderRadius.circular(10)),
-        ),
-        Positioned(
-          top: 10,
-          right: 10,
-          child: Container(
-            width: 60,
-            height: 30,
-            decoration: BoxDecoration(boxShadow: const [
-              BoxShadow(
-                  offset: Offset(0, 4),
-                  blurRadius: 4,
-                  color: Color.fromARGB(33, 0, 0, 0))
-            ], color: Colors.white, borderRadius: BorderRadius.circular(10)),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "5",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.pink),
-                ),
-                SizedBox(width: 5),
-                Icon(
-                  Icons.star,
-                  size: 18,
-                  color: Colors.pink,
-                )
-              ],
-            ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const PostDetailsPage()));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Stack(children: [
+          Container(
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 227, 225),
+                borderRadius: BorderRadius.circular(10)),
           ),
-        )
-      ]),
+          Positioned(
+            top: 10,
+            right: 10,
+            child: Container(
+              width: 60,
+              height: 30,
+              decoration: BoxDecoration(boxShadow: const [
+                BoxShadow(
+                    offset: Offset(0, 4),
+                    blurRadius: 4,
+                    color: Color.fromARGB(33, 0, 0, 0))
+              ], color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "5",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.pink),
+                  ),
+                  SizedBox(width: 5),
+                  Icon(
+                    Icons.star,
+                    size: 18,
+                    color: Colors.pink,
+                  )
+                ],
+              ),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
