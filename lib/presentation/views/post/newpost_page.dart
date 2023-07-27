7030e0f2a08e0823b8e519764a6698b4ab4ec179
Vitória <vick_santos_vick@hotmail.com>
2023-07-26 22:26:14 -0300

    --- começando a popular a página de new post

--- lib/presentation/views/post/newpost_page.dart
+++ lib/presentation/views/post/newpost_page.dart
@@ -10,9 +10,47 @@ class NewPostPage extends StatefulWidget {
 class _NewPostPageState extends State<NewPostPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Container(
+                width: size.width,
+                height: 450,
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 223, 222, 222),
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.panorama,
+                      size: 128,
+                      color: Colors.grey,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Text(
+                        "Toque aqui para selecionar a foto do seu post",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontStyle: FontStyle.italic, fontSize: 18),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+              ),
+              Container(),
+            ],
+          ),
+        ),
       ),
     );
   }
