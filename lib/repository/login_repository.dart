7150307f163ca23af045261346f3abf77eaa1938
Vitória <vick_santos_vick@hotmail.com>
2023-07-26 22:25:52 -0300

    --- alterando retorno do método de acesso

--- lib/repository/login_repository.dart
+++ lib/repository/login_repository.dart
@@ -1,10 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
+import '../models/login/acess_model.dart';
 import '../models/login/login_model.dart';
-import '../models/user/user_model.dart';
 
 class LoginRepository {
-  Future<Either<Exception, User>> logar(LoginModel login) async {
+  Future<Either<Exception, AcessModel>> logar(LoginModel login) async {
     try {
       String url = "http://192.168.2.104:8000/acesso";
 
@@ -13,8 +13,9 @@ class LoginRepository {
         "content-type": "application/json",
         "accept": "application/json",
       });
+
       if (response.statusCode == 200) {
-        return Right(User.fromJson(response.body));
+        return Right(AcessModel.fromJson(response.body));
       }
 
       return Left(Exception(response.reasonPhrase));
