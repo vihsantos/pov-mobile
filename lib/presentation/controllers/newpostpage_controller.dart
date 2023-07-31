f30b3db7efebf9865a8236437140f761339bbaa3
Vitória <vick_santos_vick@hotmail.com>
2023-07-31 01:23:43 -0300

    --- criando de fato a controller de novo post

--- lib/presentation/controllers/newpostpage_controller.dart
+++ lib/presentation/controllers/newpostpage_controller.dart
@@ -1,3 +1,26 @@
-class NewPostPageController{
-  
+import 'package:flutter/material.dart';
+import 'package:pov/repository/post_repository.dart';
+
+import '../../dto/novopost_dto.dart';
+import '../../services/error/applicationerror.dart';
+
+class NewPostPageController {
+  final PostRepository repository;
+  NovoPostDTO novoPost = NovoPostDTO();
+
+  set _error(ApplicationError? error) => solicitacaoErrorApi.value = error;
+  ApplicationError? get error => solicitacaoErrorApi.value;
+  final solicitacaoErrorApi = ValueNotifier<ApplicationError?>(null);
+
+  NewPostPageController({required this.repository});
+
+  Future criarPost() async {
+    try {
+      novoPost.user_id = 0;
+      novoPost.data_criacao = DateTime.now();
+      repository.criarPost(novoPost);
+    } on ApplicationError catch (e) {
+      _error = e;
+    }
+  }
 }
