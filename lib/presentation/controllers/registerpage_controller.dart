b289904e8f27b688289f29217726447780fdf09e
Vitória <vick_santos_vick@hotmail.com>
2023-07-31 01:24:00 -0300

    --- alguns ajustes na controller de cadastro

--- lib/presentation/controllers/registerpage_controller.dart
+++ lib/presentation/controllers/registerpage_controller.dart
@@ -1,12 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:pov/dto/novousuario_dto.dart';
 
 import '../../repository/user_repository.dart';
+import '../../services/error/applicationerror.dart';
 
 class RegisterPageController {
   NovoUsuarioDTO usuario = NovoUsuarioDTO();
-  final UserRepository userRepository;
+  final UserRepository repository;
 
-  RegisterPageController(this.userRepository);
+  set _error(ApplicationError? error) => solicitacaoErrorApi.value = error;
+  ApplicationError? get error => solicitacaoErrorApi.value;
+  final solicitacaoErrorApi = ValueNotifier<ApplicationError?>(null);
 
-  Future cadastrarUsuario() async {}
+  RegisterPageController({required this.repository});
+
+  Future cadastrarUsuario() async {
+    try {
+      var result = await repository.criarUsuario(usuario);
+      return result;
+    } on ApplicationError catch (e) {
+      _error = e;
+    }
+  }
 }
