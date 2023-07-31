c2597ee1372768e3afacf70bc1c846845ce70e6f
Vitória <vick_santos_vick@hotmail.com>
2023-07-31 01:23:03 -0300

    --- refactorying na controller de login

--- lib/presentation/controllers/loginpage_controller.dart
+++ lib/presentation/controllers/loginpage_controller.dart
@@ -1,23 +1,27 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:developer';
-
+import 'package:flutter/material.dart';
+import 'package:pov/services/singleton/auth_singleton.dart';
 import '../../models/login/login_model.dart';
-import '../../repository/login_repository.dart';
+import '../../services/error/applicationerror.dart';
 
 class LoginPageController {
-  final LoginRepository loginRepository;
+  final AuthSingleton authSingleton;
   LoginPageController({
-    required this.loginRepository,
+    required this.authSingleton,
   });
+
+  set _error(ApplicationError? error) => solicitacaoErrorApi.value = error;
+  ApplicationError? get error => solicitacaoErrorApi.value;
+  final solicitacaoErrorApi = ValueNotifier<ApplicationError?>(null);
 
   LoginModel model = LoginModel();
 
   Future logar() async {
     try {
-      var user = await loginRepository.logar(model);
-      log(user.toString());
-    } catch (e) {
-      throw Exception();
+      var user = await authSingleton.authenticate(model);
+      return user;
+    } on ApplicationError catch (e) {
+      _error = e;
     }
   }
 }
