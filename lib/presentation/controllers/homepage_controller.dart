8aeb972f2c14422291c28e557a8a2df36a36dfb0
Vitória <vick_santos_vick@hotmail.com>
2023-08-06 23:54:52 -0300

    --- criação da controller da home

---
+++ lib/presentation/controllers/homepage_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../../dto/post_dto.dart';
+import '../../repository/post_repository.dart';
+import '../../services/error/applicationerror.dart';
+
+class HomePageController {
+  final PostRepository postRepository;
+
+  HomePageController(this.postRepository);
+
+  set _error(ApplicationError? error) => solicitacaoErrorApi.value = error;
+  ApplicationError? get error => solicitacaoErrorApi.value;
+  final solicitacaoErrorApi = ValueNotifier<ApplicationError?>(null);
+
+  Future<List<PostDTO?>> listarPosts() async {
+    _error = null;
+    try {
+      List<PostDTO?> posts = await postRepository.getPosts();
+      return posts;
+    } on ApplicationError catch (e) {
+      _error = e;
+      rethrow;
+    }
+  }
+}
