a5b32a8b3d0cb563f06bf1ca3270a18c0cd86298
Vitória <vick_santos_vick@hotmail.com>
2023-08-08 21:54:25 -0300

    --- ajustes na página de cadastro

--- lib/presentation/views/register/register_page.dart
+++ lib/presentation/views/register/register_page.dart
@@ -154,6 +154,11 @@ class _RegisterPageState extends State<RegisterPage> {
                         ),
                         InkWell(
                           onTap: () {
+                            if (!isGuide) {
+                              controller.jumpToPage(3);
+                              return;
+                            }
+
                             controller.nextPage(
                                 duration: const Duration(seconds: 2),
                                 curve: Curves.ease);
@@ -190,6 +195,45 @@ class _RegisterPageState extends State<RegisterPage> {
                         )
                       ],
                     ),
+                    Column(children: [
+                      const Text("Foi?"),
+                      InkWell(
+                        onTap: () {
+                          controller.nextPage(
+                              duration: const Duration(seconds: 2),
+                              curve: Curves.ease);
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.all(15),
+                          width: size.width,
+                          height: 60,
+                          decoration: const BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                    offset: Offset(0, 4),
+                                    blurRadius: 4,
+                                    color: Color.fromARGB(55, 116, 116, 116))
+                              ],
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomLeft,
+                                  colors: [
+                                    Color.fromARGB(255, 105, 96, 232),
+                                    Color.fromARGB(255, 97, 232, 214)
+                                  ])),
+                          child: const Center(
+                              child: Text(
+                            "Próximo",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600),
+                          )),
+                        ),
+                      )
+                    ]),
                     Column(
                       children: [
                         const SizedBox(
