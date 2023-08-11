fc87c7108da8e8a680fe9481b43305bdfcab71ab
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-08-10 22:52:18 -0300

    --- listando posts por usuario

--- lib/repository/post_repository.dart
+++ lib/repository/post_repository.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart';
 import 'package:pov/dto/novopost_dto.dart';
 import 'package:http/http.dart' as http;
 import 'package:pov/models/post/post_model.dart';
+import 'package:pov/models/post/postprofile_model.dart';
 import 'package:pov/repository/login_repository.dart';
 import 'package:pov/services/singleton/auth_singleton.dart';
 
@@ -33,7 +34,7 @@ class PostRepository {
     }
   }
 
-  Future criarNovoPost(MultipartFile pic, NovoPostDTO model) async {
+  Future<bool> criarNovoPost(MultipartFile pic, NovoPostDTO model) async {
     final uri = Uri.parse("http://192.168.2.101:8000/teste");
     String? token = AuthSingleton(LoginRepository()).getToken();
 
@@ -97,6 +98,34 @@ class PostRepository {
         Iterable lista = json.decode(response.body);
         List<PostDTO> posts =
             lista.map((model) => PostDTO.fromJson(model)).toList();
+
+        return posts;
+      }
+
+      throw ApplicationErrorImp(
+          mensagem: response.reasonPhrase.toString(),
+          statusCode: response.statusCode);
+    } catch (e) {
+      throw ApplicationErrorImp(mensagem: e.toString(), statusCode: 500);
+    }
+  }
+
+  Future<List<PostProfileMode>> getPostsByUser(int id) async {
+    try {
+      String url = "http://192.168.2.101:8000/posts";
+
+      String? token = AuthSingleton(LoginRepository()).getToken();
+
+      var response = await http.get(Uri.parse(url), headers: {
+        "content-type": "application/json",
+        "accept": "application/json",
+        'Authorization': 'Bearer $token',
+      });
+
+      if (response.statusCode == 200) {
+        Iterable lista = json.decode(response.body);
+        List<PostProfileMode> posts =
+            lista.map((model) => PostProfileMode.fromJson(model)).toList();
 
         return posts;
       }
