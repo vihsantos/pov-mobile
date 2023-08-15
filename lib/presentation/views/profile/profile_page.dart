87b0fb67341a6ebf9d33f527432033ca320050b5
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-08-14 23:32:04 -0300

    --- transformando em uma página de ponte

--- lib/presentation/views/profile/profile_page.dart
+++ lib/presentation/views/profile/profile_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:pov/presentation/views/profile/components/profile_header.dart';
-
-import '../../widgets/bottom_navigation.dart';
-import 'components/card_postprofile.dart';
+import 'package:pov/presentation/views/profile/profileguide_page.dart';
+import 'package:pov/presentation/views/profile/profileperson_page.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  final bool isGuide;
+  const ProfilePage({super.key, required this.isGuide});
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -14,38 +13,6 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        bottomNavigationBar: const BottomNavigation(),
-        body: Column(
-          children: [
-            const SizedBox(height: 15),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: ProfileHeader(),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: GridView.count(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: false,
-                padding: const EdgeInsets.all(10),
-                crossAxisCount: 2,
-                children: const [
-                  CardPostProfile(),
-                  CardPostProfile(),
-                  CardPostProfile(),
-                  CardPostProfile(),
-                  CardPostProfile()
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+    return widget.isGuide? const ProfileGuidePage() : const ProfilePersonPage();
   }
 }
