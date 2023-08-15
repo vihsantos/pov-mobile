4c668a764eaff1a38b6a946aa302cac7cf3cfa82
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-08-14 23:35:18 -0300

    --- adicionando cadastro de usuário

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -1,22 +1,28 @@
+import 'dart:developer';
 
+import 'package:pov/dto/novousuario_dto.dart';
+import 'package:http/http.dart' as http;
+import '../services/error/applicationerrorimp.dart';
 
 class UserRepository {
-  // Future<Either<Exception, User>> criarUsuario(NovoUsuarioDTO usuario) async {
-  //   try {
-  //     String url = "http://192.168.2.104:8000/criarusuario";
 
-  //     var response =
-  //         await http.post(Uri.parse(url), body: usuario.toJson(), headers: {
-  //       "content-type": "application/json",
-  //       "accept": "application/json",
-  //     });
-  //     if (response.statusCode == 200) {
-  //       return Right(User.fromJson(response.body));
-  //     }
+  Future criarUsuario(NovoUsuarioDTO usuario) async {
+    try {
+      String url = "http://192.168.2.101:8000/criarusuario";
 
-  //     return Left(Exception(response.reasonPhrase));
-  //   } catch (e) {
-  //     return Left(Exception(e));
-  //   }
-  // }
+      var response =
+          await http.post(Uri.parse(url), body: usuario.toJson(), headers: {
+          "content-type": "application/json",
+          "accept": "application/json",
+      });
+
+      if(response.statusCode == 201) log("FOI!!");
+
+      throw ApplicationErrorImp(
+          mensagem: response.reasonPhrase.toString(),
+          statusCode: response.statusCode);
+    } catch (e) {
+      throw ApplicationErrorImp(mensagem: e.toString(), statusCode: 500);
+    }
+  }
 }
