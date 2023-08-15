ff8c1178c5c28b51ba2d6ec011d79d57856b69e7
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-08-14 23:32:46 -0300

    ---  ajustando a profile de guia

--- lib/presentation/views/profile/profileguide_page.dart
+++ lib/presentation/views/profile/profileguide_page.dart
@@ -11,11 +11,36 @@ class ProfileGuidePage extends StatefulWidget {
 class _ProfileGuidePageState extends State<ProfileGuidePage> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: EdgeInsets.all(15),
-          child: Column(children: [ProfileHeader()]),
+          padding: const EdgeInsets.all(15),
+          child: Column(children: [
+            const ProfileHeader(),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 90,
+                  height: 90,
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                Container(
+                  width: 90,
+                  height: 90,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ],
+            )
+          ]),
         ),
       ),
     );
