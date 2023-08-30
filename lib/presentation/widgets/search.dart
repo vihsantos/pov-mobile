14adf59fb1dbe1759e569b4ea0b310257620a24a
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-08-29 23:05:12 -0300

    --- criando o search

--- lib/presentation/widgets/search.dart
+++ lib/presentation/widgets/search.dart
@@ -1,32 +1,53 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:open_street_map_search_and_pick/open_street_map_search_and_pick.dart';
+import 'package:pov/presentation/controllers/newpostpage_controller.dart';
 
 class Search extends StatefulWidget {
-  const Search({super.key});
+  final NewPostPageController controller;
+  const Search({super.key, required this.controller});
 
   @override
   State<Search> createState() => _SearchState();
 }
 
 class _SearchState extends State<Search> {
+
+  Position? position;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_){
+    getLocation();
+  });
+    super.initState();
+  }
+
+  getLocation() async {
+    position = await widget.controller.getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: OpenStreetMapSearchAndPick(
-          buttonTextStyle:
-              const TextStyle(fontSize: 18, fontStyle: FontStyle.normal),
-          center: const LatLong(23, 89),
-          buttonColor: Colors.blue,
-          buttonText: 'Escolha a sua localização atual',
-          onPicked: (pickedData) {
-            print(pickedData.latLong.latitude);
-            print(pickedData.latLong.longitude);
-            print(pickedData.address);
-            print(pickedData.addressName);
-          },
-        )
+        buttonTextStyle:
+            const TextStyle(
+              fontSize: 18, 
+              fontStyle: FontStyle.normal),
+        center: const LatLong(-12.9704, -38.5124),
+        buttonColor: Colors.blue,
+        buttonText: 'Escolha a sua localização atual',
+        onPicked: (pickedData) {
+          print(pickedData.latLong.latitude);
+          print(pickedData.latLong.longitude);
+          print(pickedData.address);
+          print(pickedData.addressName);
+
+          widget.controller.novoPost.localizacao = "${pickedData.latLong.latitude};${pickedData.latLong.longitude}";
+        },
+      )
     );
   }
 }
