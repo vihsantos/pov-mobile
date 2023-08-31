c005af1519eac418f6cf732782018df0f3828a1e
vitoriasantos <vitoria.santos@salvador.ba.gov.br>
2023-08-31 12:57:02 -0300

    --- ajustes na página de ranking

--- lib/presentation/views/ranking/ranking_page.dart
+++ lib/presentation/views/ranking/ranking_page.dart
@@ -10,13 +10,32 @@ class RankingPage extends StatefulWidget {
 class _RankingPageState extends State<RankingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Column(
-          children: [
-            Text("TOP 5")
-          ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: const Text(
+              "Ranking",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+            ),
+          ),
+        body:  const SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Text("5 ESTRELAS",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18
+                ),),
+              ],
+            ),
+          ),
         ),
       ),
     );
