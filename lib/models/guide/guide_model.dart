c902d3064274141dc94f9634c7a0b4778109b0dd
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-09-03 00:40:01 -0300

    --- ajustes na model de guia

--- lib/models/guide/guide_model.dart
+++ lib/models/guide/guide_model.dart
@@ -1,31 +1,20 @@
 // ignore_for_file: non_constant_identifier_names
-
-import 'dart:convert';
-
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class GuideModel {
-  String? areaatuacao;
-  String? cod_cadastur;
-  GuideModel({
-    this.areaatuacao,
-    this.cod_cadastur,
-  });
+ String? areaatuacao;
+  String? codCadastur;
 
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'areaatuacao': areaatuacao,
-      'cod_cadastur': cod_cadastur,
-    };
+  GuideModel({this.areaatuacao, this.codCadastur});
+
+  GuideModel.FromJson(Map<String, dynamic> json) {
+    areaatuacao = json['areaatuacao'];
+    codCadastur = json['cod_cadastur'];
   }
 
-  factory GuideModel.fromMap(Map<String, dynamic> map) {
-    return GuideModel(
-      areaatuacao: map['areaatuacao'] != null ? map['areaatuacao'] as String : null,
-      cod_cadastur: map['cod_cadastur'] != null ? map['cod_cadastur'] as String : null,
-    );
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['areaatuacao'] = areaatuacao;
+    data['cod_cadastur'] = codCadastur;
+    return data;
   }
-
-  String toJson() => json.encode(toMap());
-
-  factory GuideModel.fromJson(String source) => GuideModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
