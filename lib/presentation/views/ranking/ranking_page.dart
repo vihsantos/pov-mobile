df4357921d12e4c12dfb95c76bf0e632392434a2
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-09-04 00:50:10 -0300

    --- montando o card para a página de ranking

--- lib/presentation/views/ranking/ranking_page.dart
+++ lib/presentation/views/ranking/ranking_page.dart
@@ -13,32 +13,113 @@ class _RankingPageState extends State<RankingPage> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            centerTitle: true,
-            title: const Text(
-              "Ranking",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-            ),
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            "Ranking",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
           ),
-        body:  const SingleChildScrollView(
+        ),
+        body: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Padding(
-            padding: EdgeInsets.all(15),
+            padding: const EdgeInsets.all(15),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("5 ESTRELAS",
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 18
-                ),),
+                const Text("5 ESTRELAS",
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 22)),
+                Container(
+                  width: 290,
+                  height: 360,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              "https://www.voltologo.net/wp-content/uploads/2022/12/pelourinho-salvador-bahia.jpg"))),
+                  child: Stack(children: [
+                    Positioned(
+                      left: 0,
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        width: 300,
+                        height: 100,
+                        decoration: const BoxDecoration(
+                            color: Color.fromARGB(178, 255, 255, 255),
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10))),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    width: 50,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(50),
+                                        color: Colors.blueGrey[900]),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 10,
+                      right: 10,
+                      child: Container(
+                        width: 60,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            boxShadow: const [
+                              BoxShadow(
+                                  offset: Offset(0, 4),
+                                  blurRadius: 4,
+                                  color: Color.fromARGB(33, 0, 0, 0))
+                            ],
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              '5',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.pink),
+                            ),
+                            SizedBox(width: 5),
+                            Icon(
+                              Icons.star,
+                              size: 18,
+                              color: Colors.pink,
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ]),
+                )
               ],
             ),
           ),
         ),
       ),
     );
-
   }
 }
