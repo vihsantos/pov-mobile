93cb2723b7ca19164664edda11fc743c2c2e0606
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-09-04 00:34:52 -0300

    --- criando a página de guias

---
+++ lib/presentation/views/trails/trails_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TrailsPage extends StatefulWidget {
+  const TrailsPage({super.key});
+
+  @override
+  State<TrailsPage> createState() => _TrailsPageState();
+}
+
+class _TrailsPageState extends State<TrailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(),
+    );
+  }
+}
