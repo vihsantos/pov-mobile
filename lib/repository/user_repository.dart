03b969fffb28f594edd8d4e06c6b87766c751685
Vitória <vick_santos_vick@hotmail.com>
2023-10-09 22:35:01 -0300

    --- criação do método de buscar dados perfil

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -2,7 +2,10 @@ import 'dart:developer';
 
 import 'package:pov/dto/novousuario_dto.dart';
 import 'package:http/http.dart' as http;
+import '../dto/dadosperfil_dto.dart';
 import '../services/error/applicationerrorimp.dart';
+import '../services/singleton/auth_singleton.dart';
+import 'login_repository.dart';
 
 class UserRepository {
   Future criarUsuario(NovoUsuarioDTO usuario) async {
@@ -16,6 +19,30 @@ class UserRepository {
       });
 
       if (response.statusCode == 201) log("FOI!!");
+
+      throw ApplicationErrorImp(
+          mensagem: response.reasonPhrase.toString(),
+          statusCode: response.statusCode);
+    } catch (e) {
+      throw ApplicationErrorImp(mensagem: e.toString(), statusCode: 500);
+    }
+  }
+
+  Future<DadosPerfilDTO> buscarDadosPerfil(int id) async {
+    try {
+      String url = "http://192.168.2.105:8000/usuario/$id";
+
+      //String? token = AuthSingleton(LoginRepository()).getToken();
+
+      var response = await http.get(Uri.parse(url), headers: {
+        "content-type": "application/json",
+        "accept": "application/json",
+      });
+
+      if (response.statusCode == 200) {
+        log(response.body);
+        return DadosPerfilDTO.fromJson(response.body);
+      }
 
       throw ApplicationErrorImp(
           mensagem: response.reasonPhrase.toString(),
