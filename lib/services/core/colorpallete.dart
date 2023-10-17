d91cfd16b2a1f1a129752cc3b161c54514814960
vitoriasantos <vitoria.santos@salvador.ba.gov.br>
2023-10-17 09:52:08 -0300

    --- adicionando a paleta de cores

---
+++ lib/services/core/colorpallete.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorPallete{
+  static const Color bgColor = Color(0xFFFFFFFF);
+  static const Color primaryColor = Color(0xFF6461E8);
+  static const Color labelColor = Color(0xFF393434);
+  static const Color bottomUnselectedColor = Color(0xFFC0C0C0);
+}
