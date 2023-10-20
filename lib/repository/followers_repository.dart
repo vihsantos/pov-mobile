9776c44b7bf7d4ae512162a1669be0aae7f9aaf9
vitoriasantos <vitoria.santos@salvador.ba.gov.br>
2023-10-20 12:55:09 -0300

    --- criação do repository de followers

---
+++ lib/repository/followers_repository.dart
@@ -0,0 +1,50 @@
+import '../services/error/applicationerrorimp.dart';
+import '../services/singleton/auth_singleton.dart';
+import 'login_repository.dart';
+import 'package:http/http.dart' as http;
+
+class FollowersRepository {
+  Future follow() async {
+    try {
+      String url = "http://172.22.17.34:8000/";
+
+      String? token = AuthSingleton(LoginRepository()).getToken();
+
+      var response = await http.get(Uri.parse(url), headers: {
+        "content-type": "application/json",
+        "accept": "application/json",
+        'Authorization': 'Bearer $token',
+      });
+
+      if (response.statusCode == 200) {}
+
+      throw ApplicationErrorImp(
+          mensagem: response.reasonPhrase.toString(),
+          statusCode: response.statusCode);
+    } catch (e) {
+      throw ApplicationErrorImp(mensagem: e.toString(), statusCode: 500);
+    }
+  }
+
+  Future unfollow() async{
+    try {
+      String url = "http://172.22.17.34:8000/";
+
+      String? token = AuthSingleton(LoginRepository()).getToken();
+
+      var response = await http.get(Uri.parse(url), headers: {
+        "content-type": "application/json",
+        "accept": "application/json",
+        'Authorization': 'Bearer $token',
+      });
+
+      if (response.statusCode == 200) {}
+
+      throw ApplicationErrorImp(
+          mensagem: response.reasonPhrase.toString(),
+          statusCode: response.statusCode);
+    } catch (e) {
+      throw ApplicationErrorImp(mensagem: e.toString(), statusCode: 500);
+    }
+  }
+}
