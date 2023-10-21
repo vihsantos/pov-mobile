dd304ebe4bc2d48300b488f16c19873311881cfe
Vitória <vick_santos_vick@hotmail.com>
2023-10-20 23:18:47 -0300

    --- adicionando itens no routes

--- lib/services/core/routes.dart
+++ lib/services/core/routes.dart
@@ -1,6 +1,7 @@
-class Routes{
-  static const String urlPartial = "";
+class Routes {
+  static const String urlPartial = "http://192.168.2.105:8000";
 
-  static const String login = "/acesso";
-
+  static const String login = "$urlPartial/acesso";
+  static const String novoUsuario = "$urlPartial/criarusuario";
+  static const String usuario = "$urlPartial/usuario/";
 }
