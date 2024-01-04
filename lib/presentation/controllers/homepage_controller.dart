a460f267bc2e6b26409dc0e3fc7ecb33fa92b8d3
vitoriasantos <vitoria.santos@salvador.ba.gov.br>
2024-01-04 13:01:51 -0300

    --- adicionando listagem de trilhas

--- lib/presentation/controllers/homepage_controller.dart
+++ lib/presentation/controllers/homepage_controller.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:pov/dto/trail_dto.dart';
+import 'package:pov/repository/trail_repository.dart';
 import '../../dto/post_dto.dart';
 import '../../repository/post_repository.dart';
 import '../../services/error/applicationerror.dart';
 
 class HomePageController {
   final PostRepository postRepository;
+  final TrailRepository trailRepository;
 
-  HomePageController(this.postRepository);
+  HomePageController(this.postRepository, this.trailRepository);
 
   set _error(ApplicationError? error) => solicitacaoErrorApi.value = error;
   ApplicationError? get error => solicitacaoErrorApi.value;
@@ -17,6 +20,17 @@ class HomePageController {
     try {
       List<PostDTO?> posts = await postRepository.getPosts();
       return posts;
+    } on ApplicationError catch (e) {
+      _error = e;
+    }
+    return null;
+  }
+
+  Future<List<TrailDTO?>?> listarTrilhas() async {
+    _error = null;
+    try {
+      List<TrailDTO?> trilhas = await trailRepository.getTrilhas();
+      return trilhas;
     } on ApplicationError catch (e) {
       _error = e;
     }
