c3bc8fa3eae00bdb37a8d8cdc74f2f981969daf8
vitoriasantos <vitoria.santos@salvador.ba.gov.br>
2024-01-05 13:03:18 -0300

    --- ajustando o card de trilhas

--- lib/presentation/views/trails/components/trailcard.dart
+++ lib/presentation/views/trails/components/trailcard.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:pov/dto/trail_dto.dart';
+import '../../../../models/enums/AreaAtuacao.dart';
 import '../../../../services/core/colorpallete.dart';
 
 class TrailCard extends StatelessWidget {
@@ -14,30 +15,69 @@ class TrailCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    //var urls = trilha.files!.split(";");
+    var urls = trilha.files!.split(";");
+    urls.removeLast();
+
+    var numAreas = trilha.occupation!.split(";");
+    List<AreaAtuacao> areaatuacao = List.empty(growable: true);
+    String areaas = "";
+
+    for (var area in numAreas) {
+      String numero = area.trim();
+
+      if (numero.isNotEmpty) {
+        AreaAtuacao a = AreaAtuacao.values.elementAt(int.parse(area.trim()));
+        areaatuacao.add(a);
+      }
+    }
+
+    for (var element in areaatuacao) {
+      areaas += "${element.descricao} - ";
+    }
+
+    areaas = areaas.substring(0, areaas.length - 2);
+
     return Container(
-      margin: const EdgeInsets.all(15),
       padding: const EdgeInsets.all(10),
-      width: size.width,
-      height: size.height * 0.34,
+      width: size.width * 0.9,
+      height: size.height * 0.3,
       decoration: BoxDecoration(
           color: ColorPallete.bgItemColor,
           borderRadius: BorderRadius.circular(10)),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Center(
-            child: Text(
-              trilha.name!,
-              style: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
-                  color: ColorPallete.labelColor),
-            ),
+          SizedBox(
+            height: size.height * 0.16,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.all(8),
+                itemCount: urls.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SizedBox(
+                      width: size.width * 0.35,
+                      child: Image.network(
+                        urls[index],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                }),
           ),
-          const Text(
-            "AREA DE ATUAÇÃO, AREA DE ATUAÇÃO, AREA DE ATUAÇÃO, AREA DE ATUAÇÃO, AREA DE ATUAÇÃO, AREA DE ATUAÇÃO",
+          Text(
+            trilha.name!,
+            style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+                color: ColorPallete.labelColor),
+          ),
+          Text(
+            areaas,
             maxLines: 2,
-            style: TextStyle(
+            style: const TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 10,
                 color: ColorPallete.labelColor),
@@ -47,7 +87,7 @@ class TrailCard extends StatelessWidget {
           ),
           Text(
             trilha.description!,
-            maxLines: 3,
+            maxLines: 2,
             style: const TextStyle(
                 color: ColorPallete.primaryColor,
                 fontSize: 12,
