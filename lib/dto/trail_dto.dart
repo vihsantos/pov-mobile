4b39274777fd52dc941e510f8a2add4f746950e4
vitoriasantos <vitoria.santos@salvador.ba.gov.br>
2024-01-05 13:01:46 -0300

    --- ajustando a dto de trilha

--- lib/dto/trail_dto.dart
+++ lib/dto/trail_dto.dart
@@ -1,47 +1,42 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
-
 import 'package:pov/models/user/userpost_model.dart';
 
 class TrailDTO {
-  int? id;
-  String? files;
   String? description;
-  UserPostModel? user;
-  String? occupation;
+  String? files;
+  int? id;
   String? name;
-  TrailDTO({
-    this.id,
-    this.files,
-    this.description,
-    this.user,
-    this.occupation,
-    this.name,
-  });
+  String? occupation;
+  UserPostModel? user;
 
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id,
-      'files': files,
-      'description': description,
-      'user': user?.toJson(),
-      'occupation': occupation,
-      'name': name,
-    };
+  TrailDTO(
+      {this.description,
+      this.files,
+      this.id,
+      this.name,
+      this.occupation,
+      this.user});
+
+  TrailDTO.fromJson(Map<String, dynamic> json) {
+    description = json['description'];
+    files = json['files'];
+    id = json['id'];
+    name = json['name'];
+    occupation = json['occupation'];
+    user = json['user'] != null ? UserPostModel.fromJson(json['user']) : null;
   }
 
-  factory TrailDTO.fromMap(Map<String, dynamic> map) {
-    return TrailDTO(
-      id: map['id'] != null ? map['id'] as int : null,
-      files: map['files'] != null ? map['files'] as String : null,
-      description: map['description'] != null ? map['description'] as String : null,
-      user: map['user'] != null ? UserPostModel.fromJson(map['user'] as Map<String,dynamic>) : null,
-      occupation: map['occupation'] != null ? map['occupation'] as String : null,
-      name: map['name'] != null ? map['name'] as String : null,
-    );
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['description'] = description;
+    data['files'] = files;
+    data['id'] = id;
+    data['name'] = name;
+    data['occupation'] = occupation;
+    if (user != null) {
+      data['user'] = user!.toJson();
+    }
+    return data;
   }
-
-  String toJson() => json.encode(toMap());
-
-  factory TrailDTO.fromJson(String source) => TrailDTO.fromMap(json.decode(source) as Map<String, dynamic>);
 }
+
