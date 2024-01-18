b1f39d2e45b53910a76ff1541719afa659b8dc46
Vitória <vick_santos_vick@hotmail.com>
2024-01-17 21:20:50 -0300

    --- criando o método d enviar comentário

--- lib/repository/comment_repository.dart
+++ lib/repository/comment_repository.dart
@@ -1,3 +1,29 @@
-class CommentRepository{
-  
+import 'dart:developer';
+
+import '../dto/comment_dto.dart';
+import '../services/core/routes.dart';
+import 'package:http/http.dart' as http;
+
+import '../services/error/applicationerrorimp.dart';
+
+class CommentRepository {
+  Future criarUsuario(CommentDTO comentario) async {
+    try {
+      String url = Routes.comment;
+
+      var response =
+          await http.post(Uri.parse(url), body: comentario.toJson(), headers: {
+        "content-type": "application/json",
+        "accept": "application/json",
+      });
+
+      if (response.statusCode == 201) log("FOI!!");
+
+      throw ApplicationErrorImp(
+          mensagem: response.reasonPhrase.toString(),
+          statusCode: response.statusCode);
+    } catch (e) {
+      throw ApplicationErrorImp(mensagem: e.toString(), statusCode: 500);
+    }
+  }
 }
