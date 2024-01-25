eff5dab50ec9dcb2b97a92c55bb26452b32f54e1
Vitória <vick_santos_vick@hotmail.com>
2024-01-25 00:02:50 -0300

    --- ajustando a controller de trilhas

--- lib/presentation/controllers/trailpage_controller.dart
+++ lib/presentation/controllers/trailpage_controller.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
 import 'package:pov/dto/trail_dto.dart';
 import 'package:pov/repository/trail_repository.dart';
+
+import '../../services/error/applicationerror.dart';
 
 class TrailPageController {
   TrailRepository repository;
@@ -9,12 +12,17 @@ class TrailPageController {
     required this.repository,
   });
 
-  Future<List<TrailDTO?>> listarTrilhas(int guia) async{
+  set _error(ApplicationError? error) => solicitacaoErrorApi.value = error;
+  ApplicationError? get error => solicitacaoErrorApi.value;
+  final solicitacaoErrorApi = ValueNotifier<ApplicationError?>(null);
+
+  Future<List<TrailDTO?>?> listarTrilhas(int guia) async {
+    _error = null;
     try {
       return await repository.listarTrilhasPorGuia(guia);
-    } catch (e) {
-      rethrow;
+    } on ApplicationError catch (e) {
+      _error = e;
     }
+    return null;
   }
-
 }
