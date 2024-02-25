9430e2572ed6b8e5470c26f18f1cd7f03002c252
vihsantos <vick_santos_vick@hotmail.com>
2024-02-24 22:26:50 -0300

    --- criando página de detalhes da trilha

---
+++ lib/presentation/views/trails/traildetails_page.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class TrailDetailsPage extends StatefulWidget {
+  final int id;
+  const TrailDetailsPage({
+    Key? key,
+    required this.id,
+  }) : super(key: key);
+
+  @override
+  State<TrailDetailsPage> createState() => _TrailDetailsPageState();
+}
+
+class _TrailDetailsPageState extends State<TrailDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
